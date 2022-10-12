5b83c0fa51fb49d3de84ce23e0593d89aac97040
kpyatakov <kpyatakov@gmail.com>
2022-10-12 19:24:35 +0300

    v3

--- src/test/java/ru/netology/test/CardOrderTest.java
+++ src/test/java/ru/netology/test/CardOrderTest.java
@@ -1,11 +1,18 @@
 package ru.netology.test;
 
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import org.openqa.selenium.Keys;
+
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
 	
